118d43f5dd9f7465d9326c5ec975cb97645f4963
karlavalenca0 <kvalenca493@gmail.com>
2024-06-05 11:37:20 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,27 @@
+//variáveis da bolinha
+let xBolinha = 20;
+let yBolinha = 100;
+let diametro = 20;
+let raio = diametro / 2;
+
+//variáveis do oponente
+let xRaqueteOponente = 585;
+let yRaqueteOponente = 150;
+
+//velocidade da bolinha
+let velocidadeXBolinha = 7;
+let velocidadeYBolinha = 7;
+
+//variáveis da raquete
+let xRaquete = 5;
+let yRaquete = 150;
+let raqueteComprimento = 10;
+let raqueteAltura = 90;
+
+//placar do jogo
+let meusPontos = 0;
+let pontosDoOponente = 0;
+
+
+//sons do jogo
+let raquetada;
